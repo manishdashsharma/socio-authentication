86878bbe213b76dbbe1de9c8deec3d72e8688013
Manish Dash Sharma <60460877+manishdashsharma@users.noreply.github.com>
2023-11-23 22:47:13 +0000

    Basic setup

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(cookieParser())
+
+
+app.get("/", (_req, res) => {
+    res.send("All services are running fine. Stop doubting the backend, you frontend programmer!")
+})
+
+app.all("*", (_req, res) => {
+    return res.status(404).json({
+        success: false,
+        message: "Route not found"
+    })
+})
+export default app;
